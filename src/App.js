d5a92c819ad982add682b3f60190fdd92b72a05a
bradley-allan-hopkins <125572439+bradley-allan-hopkins@users.noreply.github.com>
2023-03-12 21:19:32 -0600

    Finished Front End

--- src/App.js
+++ src/App.js
@@ -5,6 +5,7 @@ import HomePage from './pages/HomePage';
 import AboutPage from './pages/AboutPage';
 import ArticlesListPage from './pages/ArticlesListPage';
 import ArticlePage from './pages/ArticlePage';
+import NotFoundPage from './pages/NotFoundPage';
 
 function App() {
   return (
@@ -16,7 +17,8 @@ function App() {
           <Route path="/" element={<HomePage />} />
           <Route path="/about" element={<AboutPage />} />
           <Route path="/articles" element={<ArticlesListPage />} />
-          <Route path="/articles/:articleID" element={<ArticlePage />} />
+          <Route path="/articles/:articleId" element={<ArticlePage />} />
+          <Route path="*" element={<NotFoundPage />} />
         </Routes>
          
 
